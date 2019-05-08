405b489bd143760eea04d4f3a341c1004e978fb5
Jonathan Luk <jluk87@gmail.com>
2019-05-08 00:34:06 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   end
 
   def add
-    session[:cart] << params[:product]
+    cart << params[:product]
     redirect_to products_index_path
   end
 
