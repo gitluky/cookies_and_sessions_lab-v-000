ffca28a951b56a4affafee701ea295eb90f3791c
Jonathan Luk <jluk87@gmail.com>
2019-05-08 00:11:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'products/index'
 
-  get 'products/add'
+  post 'products/add'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
